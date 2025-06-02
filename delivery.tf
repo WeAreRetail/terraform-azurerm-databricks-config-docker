4f7a32a1aa1710a854f16d5ea93a31cc851f6f7d
AzureDevOps agent <AzDevOps@p10dev76c9vmsslinuxazdoagentsu2204020002E7.g1w0l4atparuppi3wihmev2psf.ax.internal.cloudapp.net>
2025-06-02 15:21:17 +0000

    Artefact release

--- delivery.tf
+++ delivery.tf
@@ -4,38 +4,48 @@ resource "databricks_directory" "delivery" {
   path = "/delivery"
 }
 
-resource "databricks_permissions" "delivery_usage" {
-  count          = var.unity_permissions_migration ? 1 : 0 # Only if migration is enabled.
-  directory_path = databricks_directory.delivery.path
+locals {
+  delivery_usage_permissions_migration = var.unity_permissions_migration ? {
+    access_control_can_edit = {
+      group_name       = databricks_group.analysts[0].display_name
+      permission_level = "CAN_EDIT"
+    }
+    access_control_can_read = {
+      group_name       = databricks_group.readonly[0].display_name
+      permission_level = "CAN_READ"
+    }
+  } : {}
 
-  access_control {
-    group_name       = databricks_group.analysts[0].display_name
-    permission_level = "CAN_EDIT"
-  }
+  delivery_usage_permissions_unity = var.unity_permissions ? {
+    access_control_can_edit_unity = {
+      group_name       = local.users_group_name
+      permission_level = "CAN_EDIT"
+    }
+    access_control_can_read_unity = {
+      group_name       = local.read_group_name
+      permission_level = "CAN_READ"
+    }
+  } : {}
 
-  access_control {
-    group_name       = databricks_group.readonly[0].display_name
-    permission_level = "CAN_READ"
-  }
+  delivery_usage_permissions_merged = merge(
+    local.delivery_usage_permissions_migration,
+    local.delivery_usage_permissions_unity
+  )
 }
 
-resource "databricks_permissions" "delivery_usage_unity" {
-  count          = var.unity_permissions ? 1 : 0 # Only if Unity permissions are enabled.
+resource "databricks_permissions" "delivery_usage" {
   directory_path = databricks_directory.delivery.path
-  depends_on     = [databricks_directory.delivery]
 
-  access_control {
-    group_name       = local.users_group_name
-    permission_level = "CAN_EDIT"
-  }
-
-  access_control {
-    group_name       = local.read_group_name
-    permission_level = "CAN_READ"
+  dynamic "access_control" {
+    for_each = local.delivery_usage_permissions_merged
+    content {
+      group_name       = access_control.value["group_name"]
+      permission_level = access_control.value["permission_level"]
+    }
   }
 }
 
 moved {
-  from = databricks_permissions.delivery_usage
-  to   = databricks_permissions.delivery_usage[0]
+  from = databricks_permissions.delivery_usage[0]
+  to   = databricks_permissions.delivery_usage
 }
